a478837bd6e66808ac99f58a69cb1a6365822bb0
sri12334 <srir48747@gmail.com>
2023-11-20 13:39:24 +0100

    Revert "Revert "code changed""
    This reverts commit 844b8783522f1a58987a587a3f0373555acfdf79.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,26 @@
 module.exports = {
-  extends: ['eslint:recommended'],
-  plugins: ['html'],
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-
-    {
-      files: ['**/*.html'],
-      processor: 'html/html',
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
-};
+    "rules": {
+    }
+}
